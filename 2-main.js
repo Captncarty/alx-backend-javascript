499f24ad3d6c7eb66ac6c512098826d58a1e5a2a
Kelvin <edithmukoro3@gmail.com>
2023-06-30 00:56:45 +0100

    task 00

--- 2-main.js
+++ 2-main.js
@@ -1,11 +1,4 @@
-import createEmployeesObject from './0x00-ES6_basic/11-createEmployeesObject.js';
-import createReportObject from './0x00-ES6_basic/12-createReportObject.js';
+import ClassRoom from "./0x02-ES6_classes/0-classroom";
 
-const employees = {
-    ...createEmployeesObject('engineering', ['Bob', 'Jane']),
-    ...createEmployeesObject('marketing', ['Sylvie'])
-};      
-
-const report = createReportObject(employees);
-console.log(report.allEmployees);
-console.log(report.getNumberOfDepartments(report.allEmployees));
+const room = new ClassRoom(10);
+console.log(room._maxStudentsSize)
